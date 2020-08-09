860357ef164bb0e1a8276cb85f7ecb83cc24186a
ViolinLee <mechleechan@outlook.com>
2020-08-09 23:58:24 +0800

    first commit

---
+++ mega_hexapod/mega_hexapod.ino
@@ -0,0 +1,12 @@
+// Hexapod Code for Arduino Mega by LeeChan
+
+# include <Servo.h>
+# include <math.h>
+
+// Servos Declarations: coxal --> femurl --> tibia
+Servo ServosGroup0[3];
+Servo ServosGroup1[3];
+Servo ServosGroup2[3];
+Servo ServosGroup3[3];
+Servo ServosGroup4[3];
+Servo ServosGroup5[3];
